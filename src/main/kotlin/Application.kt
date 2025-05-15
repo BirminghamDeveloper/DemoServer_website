936f00b879a879bc8b3dc630b3dc49fcc8b8c71f
Mustafa <smart1hashi@gmail.com>
2025-05-15 08:02:11 +0100

    + add files into the server resources

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,6 +1,8 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.resource
+import io.ktor.server.http.content.static
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.routing
 import io.ktor.server.routing.*
@@ -17,6 +19,11 @@ fun Application.module() {
 fun Application.module2(){
 //    install(CallLogging)
     routing {
+        static{
+            resource("facebook.html")
+            resource("static/file.txt")
+            resource("static/hani.jpeg")
+        }
         get("/book") {
             call.respondText("Hello, Books Sections")
         }
